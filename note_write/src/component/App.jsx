db0fb8e285dec2fbeb28f76a83a1df7628d61468
wx-5959 <wx-jinp@dtdream.com>
2022-05-17 15:09:57 +0800

    [优化]记录离开前页面高度 [优化]编辑自动聚焦

--- note_write/src/component/App.jsx
+++ note_write/src/component/App.jsx
@@ -21,7 +21,7 @@ export default class App extends Component {
     noteNavigatorList: [],
     navigatorVisble: false,
     drawerVisible: false,
-    currentCategory: ''
+    currentCategory: "",
   };
 
   writeNoteRef = React.createRef();
@@ -30,9 +30,16 @@ export default class App extends Component {
   formRef = React.createRef();
   componentDidMount() {
     this.hundleDocumentAddEventListener();
-    this.renderNote(localStorage.getItem('currentTitle'));
-    this.renderNavigator(localStorage.getItem('currentTitle'));
+    this.renderNote(localStorage.getItem("currentTitle"));
+    this.renderNavigator(localStorage.getItem("currentTitle"));
+    this.contentScrollToHistoryVisit();
   }
+
+  contentScrollToHistoryVisit = () => {
+    setTimeout(() => {
+      this.appRef.current.scrollTo(0, Number(localStorage.getItem("scroll")));
+    });
+  };
 
   openNoteCategoryDrawer = () => {
     const { drawerVisible } = this.state;
@@ -43,8 +50,10 @@ export default class App extends Component {
 
   // utils
   renderNavigator = (title) => {
-    const initNote = JSON.parse(storageGet("note") || "[]")
-    const note = title ? initNote.find(item => item.title === title)?.content : initNote.find(item => item.visible)?.content;
+    const initNote = JSON.parse(storageGet("note") || "[]");
+    const note = title
+      ? initNote.find((item) => item.title === title)?.content
+      : initNote.find((item) => item.visible)?.content;
     let reg = /<h3(.*)><strong>(.*)<\/strong><\/h3>/gi;
     const noteNavigatorList = [];
     note?.forEach((item) => {
@@ -52,7 +61,6 @@ export default class App extends Component {
       Array.isArray(resultList) &&
         resultList.forEach((item) => {
           const result = /<h3(.*)><strong>(.*)<\/strong><\/h3>/gi.exec(item);
-          console.log(result);
           noteNavigatorList.push(result && result[2]);
         });
     });
@@ -63,48 +71,54 @@ export default class App extends Component {
 
   // 选择篇目
   selectCategory = (title) => {
-    console.log(title);
-    this.renderNote(title)
-    this.openNoteCategoryDrawer()
-    this.renderNavigator(title)
+    this.renderNote(title);
+    this.openNoteCategoryDrawer();
+    this.renderNavigator(title);
     this.setState({
-      currentCategory: title
-    })
-  }
+      currentCategory: title,
+    });
+  };
 
   renderNote = (title) => {
-    console.log(title, 'title');
-    const initNote = JSON.parse(storageGet("note") || '[]')
-    const note = title ? initNote.find(item => item.title === title) : initNote.find(item => item.visible);
+    const initNote = JSON.parse(storageGet("note") || "[]");
+    const note = title
+      ? initNote.find((item) => item.title === title)
+      : initNote.find((item) => item.visible);
     this.setState({
       note,
-      currentCategory: note?.title
+      currentCategory: note?.title,
     });
   };
 
   deleteCurrntNoteContent = (key) => {
     const { currentCategory } = this.state;
     let currentContentIndex;
-    const currentNoteContent = JSON.parse(storageGet("note") || "[]").find((item, index) => { currentContentIndex = index; return item.title === currentCategory; })?.content;
+    const currentNoteContent = JSON.parse(storageGet("note") || "[]").find(
+      (item, index) => {
+        currentContentIndex = index;
+        return item.title === currentCategory;
+      }
+    )?.content;
     const note = JSON.parse(storageGet("note") || "[]");
     const filterNote = currentNoteContent.filter((item) => item.key !== key);
-    note.splice(currentContentIndex, 1, { title: currentCategory, content: filterNote })
+    note.splice(currentContentIndex, 1, {
+      title: currentCategory,
+      content: filterNote,
+    });
     storageSet("note", JSON.stringify(note));
     this.setState({
       note: {
         title: currentCategory,
-        content: filterNote
+        content: filterNote,
       },
     });
   };
 
   editeCurrentContentKey = (key) => {
     const { currentCategory } = this.state;
-    const currentContent = JSON.parse(
-      storageGet("note") || "[]"
-    )
-    .find(item => item.title === currentCategory)?.content
-    .find((item) => item.key === key);
+    const currentContent = JSON.parse(storageGet("note") || "[]")
+      .find((item) => item.title === currentCategory)
+      ?.content.find((item) => item.key === key);
     this.hundleModal();
     this.setState({
       editorState: BraftEditor.createEditorState(currentContent?.noteContent),
@@ -121,8 +135,13 @@ export default class App extends Component {
     const { noteVisble, editeKey, currentCategory } = this.state;
     const htmlContent = this.state.editorState.toHTML();
     let currentContentIndex;
-    const currentnote = JSON.parse(storageGet("note") || "[]").find((item, index) => { currentContentIndex = index ;return item.title === currentCategory });
-    const { content: currentContent } = currentnote
+    const currentnote = JSON.parse(storageGet("note") || "[]").find(
+      (item, index) => {
+        currentContentIndex = index;
+        return item.title === currentCategory;
+      }
+    );
+    const { content: currentContent } = currentnote;
     const note = JSON.parse(storageGet("note") || "[]");
     if (editeKey) {
       const currentEditeContent = currentContent.findIndex(
@@ -135,10 +154,11 @@ export default class App extends Component {
         key: Date.now(),
       });
     }
-    !isNaN(currentContentIndex) && note.splice(currentContentIndex, 1, currentnote)
+    !isNaN(currentContentIndex) &&
+      note.splice(currentContentIndex, 1, currentnote);
     this.setState({ note: currentnote });
     storageSet("note", JSON.stringify(note));
-    this.renderNavigator(currentCategory)
+    this.renderNavigator(currentCategory);
     this.setState({
       noteVisble: !noteVisble,
       editeKey: 0,
@@ -161,30 +181,55 @@ export default class App extends Component {
 
   hundleCataegoryModal = (type) => {
     const { categoryVisble, currentCategory } = this.state;
-    const note = JSON.parse(storageGet('note') || '[]')
-    const { getFieldValue, validateFields } = this.formRef?.current || {}
-    if(type === 'ok') {
-      validateFields(['category'])
-      .then(
-        val => {
-          note.push({
-            title: getFieldValue('category'),
-            visible: true,
-            content: []
-          })
-          storageSet('note', JSON.stringify(note))
-          this.setState({
-            categoryVisble: !categoryVisble,
-            currentCategory: currentCategory ? currentCategory : getFieldValue('category')
-          });
-        }
-      )
+    const note = JSON.parse(storageGet("note") || "[]");
+    const { getFieldValue, validateFields } = this.formRef?.current || {};
+    if (type === "ok") {
+      validateFields(["category"]).then((val) => {
+        note.push({
+          title: getFieldValue("category"),
+          visible: true,
+          content: [],
+        });
+        storageSet("note", JSON.stringify(note));
+        this.setState({
+          categoryVisble: !categoryVisble,
+          currentCategory: currentCategory
+            ? currentCategory
+            : getFieldValue("category"),
+        });
+      });
     } else {
       this.setState({
         categoryVisble: !categoryVisble,
       });
     }
-  }
+  };
+
+  // 光标自动聚焦
+  setEditeRange = () => {
+    setTimeout(() => {
+      const editeDom =
+        this.writeNoteRef.containerNode.children[1].children[0].children[0]
+          .children[0];
+      const domList = [...editeDom.children[0].childNodes];
+      const rangeDom = domList[domList.length - 1];
+      const sel = window.getSelection();
+      editeDom.focus();
+      function getLastChild(domChild) {
+        if (
+          domChild?.childNodes &&
+          domChild.childNodes[0]?.nodeName !== "#text" &&
+          [...domChild.childNodes].length
+        ) {
+          return getLastChild(domChild.childNodes[0]);
+        } else {
+          return domChild;
+        }
+      }
+      sel.extend(getLastChild(rangeDom), 1);
+      sel.collapseToEnd();
+    });
+  };
 
   hundleModal = (type) => {
     const { noteVisble } = this.state;
@@ -199,6 +244,7 @@ export default class App extends Component {
         editeKey: 0,
       });
     }
+    this.setEditeRange();
   };
 
   handleEditorChange = (editorState) => {
@@ -209,34 +255,42 @@ export default class App extends Component {
 
   setNoteCategory = (e) => {
     const { keyCode } = e;
-    if(keyCode === 13) {
-      this.hundleCataegoryModal('ok')
+    if (keyCode === 13) {
+      this.hundleCataegoryModal("ok");
     }
-  }
-  
+  };
+
   writeNote = (e) => {
     const { key, type } = e;
     const { keyCode } = this.state;
     keyCode.push(key);
     if (type === "keydown") {
       if (/alt/i.test(keyCode[0]) && /^w$/i.test(keyCode[1])) {
-        if(!storageGet('note') || !JSON.parse(storageGet('note') || '[]').find(item => item.visible)) {
+        if (
+          !storageGet("note") ||
+          !JSON.parse(storageGet("note") || "[]").find((item) => item.visible)
+        ) {
           this.setState({
             categoryVisble: true,
             keyCode: [],
-          })
+          });
           return;
         }
         this.setState({
           noteVisble: true,
         });
+        this.setEditeRange();
       }
-    } else if(/alt/i.test(keyCode[0]) && /^a$/i.test(keyCode[1])) {
-      this.setState({
-        categoryVisble: true,
-      }, () => this.setState({
-        keyCode: [],
-      }));
+    } else if (/alt/i.test(keyCode[0]) && /^a$/i.test(keyCode[1])) {
+      this.setState(
+        {
+          categoryVisble: true,
+        },
+        () =>
+          this.setState({
+            keyCode: [],
+          })
+      );
     } else {
       this.setState({
         keyCode: [],
@@ -254,14 +308,23 @@ export default class App extends Component {
       navigatorVisble,
       drawerVisible,
       categoryVisble,
-      currentCategory
+      currentCategory,
     } = this.state;
+    const controls = [
+      "font-size",
+      "text-color",
+      "bold",
+      "headings",
+      "code",
+      "text-indent",
+    ];
     return (
       <div
         className="app"
         ref={this.appRef}
         onMouseMove={this.hundlePageCoordinate}
         onMouseLeave={() => this.setState({ navigatorVisble: false })}
+        onScroll={(e) => localStorage.setItem("scroll", e.target.scrollTop)}
       >
         <div
           style={{
@@ -275,25 +338,30 @@ export default class App extends Component {
           <ExportText />
           <NoteCategory openNoteCategoryDrawer={this.openNoteCategoryDrawer} />
         </div>
-        <NoteCategoryDrawer visible={drawerVisible} openNoteCategoryDrawer={this.openNoteCategoryDrawer} selectCategory={this.selectCategory}/>
+        <NoteCategoryDrawer
+          visible={drawerVisible}
+          openNoteCategoryDrawer={this.openNoteCategoryDrawer}
+          selectCategory={this.selectCategory}
+        />
         <Modal
           title="添加篇目"
           onOk={() => this.hundleCataegoryModal("ok")}
           onCancel={this.hundleCataegoryModal}
           visible={categoryVisble}
         >
-          <Form
-            ref={this.formRef}
-          >
+          <Form ref={this.formRef}>
             <Form.Item
               name="category"
               rules={[
                 {
-                  required: true
-                }
+                  required: true,
+                },
               ]}
             >
-              <Input placeholder="输入要添加的篇目" onKeyUp={this.setNoteCategory} />
+              <Input
+                placeholder="输入要添加的篇目"
+                onKeyUp={this.setNoteCategory}
+              />
             </Form.Item>
           </Form>
         </Modal>
@@ -304,6 +372,7 @@ export default class App extends Component {
           onCancel={this.hundleModal}
         >
           <BraftEditor
+            controls={controls}
             value={editorState}
             onChange={this.handleEditorChange}
             onSave={this.submitContent}
@@ -311,56 +380,63 @@ export default class App extends Component {
           />
         </Modal>
         <div style={{ marginTop: 70 }}>
-          <div 
-            style={{ 
-              textAlign: 'center',
-              fontSize: 'xx-large',
-              fontFamily: 'cursive',
-              color: '#f52f26'
-           }}
-          >{currentCategory}</div>
-          {note?.content && note.content.map((item) => (
-            <div
-              onMouseEnter={() => this.setState({ currentKey: item?.key })}
-              onMouseLeave={() => this.setState({ currentKey: 0 })}
-              key={item?.key}
-            >
-              <Row>
-                <Col span={20}>
-                  <div
-                    dangerouslySetInnerHTML={{ __html: item?.noteContent }}
-                    style={Object.assign(
-                      {
-                        wordBreak: "break-all",
-                        minHeight: 40,
-                        border: "1px solid transparent",
-                        paddingLeft: 6,
-                      },
-                      this.isCurrentBody(item?.key)
-                        ? { background: "#fff", borderColor: "#d67f5c" }
-                        : { borderColor: "transparent" }
+          <div
+            style={{
+              textAlign: "center",
+              fontSize: "xx-large",
+              fontFamily: "cursive",
+              color: "#f52f26",
+            }}
+          >
+            {currentCategory}
+          </div>
+          {note?.content &&
+            note.content.map((item) => (
+              <div
+                onMouseEnter={() => this.setState({ currentKey: item?.key })}
+                onMouseLeave={() => this.setState({ currentKey: 0 })}
+                key={item?.key}
+              >
+                <Row>
+                  <Col span={20}>
+                    <div
+                      dangerouslySetInnerHTML={{ __html: item?.noteContent }}
+                      style={Object.assign(
+                        {
+                          wordBreak: "break-all",
+                          minHeight: 40,
+                          border: "1px solid transparent",
+                          paddingLeft: 6,
+                        },
+                        this.isCurrentBody(item?.key)
+                          ? { background: "#fff", borderColor: "#d67f5c" }
+                          : { borderColor: "transparent" }
+                      )}
+                      onDoubleClick={() =>
+                        this.editeCurrentContentKey(item?.key)
+                      }
+                    />
+                  </Col>
+                  <Col span={4}>
+                    {currentKey === item?.key ? (
+                      <>
+                        <Button
+                          danger
+                          onDoubleClick={() =>
+                            this.deleteCurrntNoteContent(item?.key)
+                          }
+                          style={{ marginBottom: 5 }}
+                        >
+                          删除
+                        </Button>
+                      </>
+                    ) : (
+                      ""
                     )}
-                    onDoubleClick={() => this.editeCurrentContentKey(item?.key)}
-                  />
-                </Col>
-                <Col span={4}>
-                  {currentKey === item?.key ? (
-                    <>
-                      <Button
-                        danger
-                        onDoubleClick={() => this.deleteCurrntNoteContent(item?.key)}
-                        style={{ marginBottom: 5 }}
-                      >
-                        删除
-                      </Button>
-                    </>
-                  ) : (
-                    ""
-                  )}
-                </Col>
-              </Row>
-            </div>
-          ))}
+                  </Col>
+                </Row>
+              </div>
+            ))}
         </div>
         <Navigator
           navigatorVisble={navigatorVisble}
